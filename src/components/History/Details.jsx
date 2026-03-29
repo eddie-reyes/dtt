85913bd17cf07ef3101ede49e62e3376da38aa04
Imtiaz Ahmed <imti.imtiaz2@gmail.com>
2026-03-28 22:31:27 -0700

    implement bootstrap prototype to history

--- src/components/History/Details.jsx
+++ src/components/History/Details.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
 import styles from "./Details.module.css";
 import { useLocation } from "react-router-dom";
 
@@ -7,31 +8,58 @@ export default function details() {
     const { patient } = location.state || {};
 
     return (
-        <div className={styles.body}>
-            <div className={styles.exit}>
-            <Link to="/history" className={styles.backButton}>
-                ← Back
-            </Link>
-        </div>
-
-        <div className={styles.title}> 
-            <h1>History</h1>
-        </div>
-
-        <div className={styles.content}>
-            <div className={styles.diagnosis}>
-                <h2>{patient.correct ? "Correct" : "Incorrect"} Diagnosis</h2>
+        <div className="grid text-center">
+            <div className="row">
+                <div className="col">
+                    <button className="btn btn-success">
+                        <Link to="/menu">
+                            ← Back
+                        </Link>
+                    </button>
+                </div>
+            
+                <div className="col">
+                    <h1>History</h1>
+                </div>
+            
+                <div className="col">
+                    <img src={logo} alt="logo" className="w-25"></img>
+                </div>
             </div>
 
-            <div className={styles.details}>
-                <h3>{patient.details}</h3>
-            </div>
-
-            <div className={styles.details}>
-                <p>Diagnosis Time: {patient.timeElapsed}</p>
+            <div className="row">
+                <div className="col">
+                    <h2>{patient.correct ? "Correct" : "Incorrect"} Diagnosis</h2>
+                    <h3>{patient.details}</h3>
+                    <p>Diagnosis Time: {patient.timeElapsed}</p>
+                </div>
             </div>
         </div>
+        // <div className={styles.body}>
+        //     <div className={styles.exit}>
+        //     <Link to="/history" className={styles.backButton}>
+        //         ← Back
+        //     </Link>
+        // </div>
+
+        // <div className={styles.title}> 
+        //     <h1>History</h1>
+        // </div>
+
+        // <div className={styles.content}>
+        //     <div className={styles.diagnosis}>
+        //         <h2>{patient.correct ? "Correct" : "Incorrect"} Diagnosis</h2>
+        //     </div>
+
+        //     <div className={styles.details}>
+        //         <h3>{patient.details}</h3>
+        //     </div>
+
+        //     <div className={styles.details}>
+        //         <p>Diagnosis Time: {patient.timeElapsed}</p>
+        //     </div>
+        // </div>
         
-        </div>
+        // </div>
     )
 }
--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -1,36 +1,73 @@
 import data from "./data/patients.json";
 import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
 import styles from "./History.module.css";
+import "../scss/styles.scss";
 
 export default function history() {
     return (
-    <div className={styles.body}>
-        <div className={styles.exit}>
-            <Link to="/menu" className={styles.backButton}>
-                ← Back
-            </Link>
-        </div>
+        <div className="grid text-center">
+            <div className="row">
+                <div className="col">
+                    <button className="btn btn-success">
+                        <Link to="/menu">
+                            ← Back
+                        </Link>
+                    </button>
+                </div>
 
-        <div className={styles.title}> 
-            <h1>History</h1>
-        </div>
+                <div className="col">
+                    <h1>History</h1>
+                </div>
 
-        <div className={styles.content}>
-            <ul className={styles.list}>
-                {data.map((patient) => (
-                    console.log(patient),
-                    console.log(typeof patient),
-                    <li key={patient.id}>
-                        <div>
-                            <p>Patient: {patient.name}: {patient.correct ? "Correct" : "Incorrect"} Diagnosis</p>
-                            <Link to={`/details/${patient.id}`} state={{ patient }}>
-                                Show Details
-                            </Link>
-                        </div>
-                    </li>
-                ))}
-            </ul>
+                <div className="col">
+                    <img src={logo} alt="logo" className="w-25"></img>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col">
+                    <ul className="list-group">
+                        {data.map((patient) => (
+                            <li key={patient.id} className="list-group-item">
+                                <p>Patient: {patient.name}: {patient.correct ? "Correct" : "Incorrect"} Diagnosis</p>
+                                <Link to={`/details/${patient.id}`} state={{ patient }}>
+                                    Show Details
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+
         </div>
-    </div>
+    // <div className={styles.body}>
+    //     <div className={styles.exit}>
+    //         <Link to="/menu" className={styles.backButton}>
+    //             ← Back
+    //         </Link>
+    //     </div>
+
+    //     <div className={styles.title}> 
+    //         <h1>History</h1>
+    //     </div>
+
+    //     <div className={styles.content}>
+    //         <ul className={styles.list}>
+    //             {data.map((patient) => (
+    //                 console.log(patient),
+    //                 console.log(typeof patient),
+    //                 <li key={patient.id}>
+    //                     <div>
+    //                         <p>Patient: {patient.name}: {patient.correct ? "Correct" : "Incorrect"} Diagnosis</p>
+    //                         <Link to={`/details/${patient.id}`} state={{ patient }}>
+    //                             Show Details
+    //                         </Link>
+    //                     </div>
+    //                 </li>
+    //             ))}
+    //         </ul>
+    //     </div>
+    // </div>
     )
 }
