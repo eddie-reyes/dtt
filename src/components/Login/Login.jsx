bcfee3f0a640d13711080c3cfbb4e5d6b4740d9c
Imtiaz Ahmed <imti.imtiaz2@gmail.com>
2026-03-29 22:10:50 -0700

    added bootstrap to login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,36 +1,36 @@
 import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
 import styles from "./Login.module.css";
+import "../scss/styles.scss";
 
 export default function login() {
     return (
-    <>
-    <div className={styles.body}>
-        <div className={styles.back}>
-            <Link to="/" className={styles.backButton}>
-                ← Back
-            </Link>
-        </div>
-        <div className={styles.title}>
-            <h1>Diagnostic Training Tool</h1>
-        </div>
+        <div className="d-flex justify-content-center align-items-center vh-100 bg-secondary">
+            <div className="w-75">
+                <div className="d-flex justify-content-between align-items-center mb-3">
+                    <button className="btn btn-success position-absolute top-0 start-0">
+                        <Link className="text-white" to="/">
+                            ← Back
+                        </Link>
+                    </button>
+                    <span className="position-absolute top-0 start-50 translate-middle-x d-flex align-items-center">
+                        <img src={logo} alt="logo" style={{ width: '128px' }}></img>
+                        <h1 className="d-inline-block ms-3">iagnostic Training Tool</h1>
+                    </span>
+                </div>
 
-        {/* <div className={styles.logo}>
-            <img src="./src/assets/logo.png" alt="" className={styles.img}/>
-        </div> */}
-
-        <form className={styles.login}>
-            <input name="username" type="text" placeholder="Username:" className={styles.username} required />
-            <input name="password" type="password" placeholder="Password:" className={styles.password} required />
-            <button type="submit" className={styles.submit}>Login</button>
-        </form>
-
-        <div className={`${styles.visittext} ${styles.text}`}>
-            <p>Visit our website for more information: <a href="https://www.diagnostictrainingtool.com">www.diagnostictrainingtool.com</a></p>
+                <form action="">
+                    <div className="form-floating mb-3">
+                        <input name="username" type="text" className="form-control" id="floatingUsername" placeholder="Username:" required />
+                        <label htmlFor="floatingUsername">Username:</label>
+                    </div>
+                    <div className="form-floating mb-3">
+                        <input name="password" type="password" className="form-control" id="floatingPassword" placeholder="Password:" required />
+                        <label htmlFor="floatingPassword">Password:</label>
+                    </div>
+                    <button type="submit" className="btn btn-primary w-100">Login</button>
+                </form>
+            </div>
         </div>
-        <div className={`${styles.copyright} ${styles.text}`}>
-            <p>Copyright © 2024 Diagnostic Training Tool. All rights reserved.</p>
-        </div>
-    </div>
-    </>
     )
 }
