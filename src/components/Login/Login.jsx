848f29b0f9ad29a44c7492895516ef16fc134554
TatianaChe <tatianacherbaeva@gmail.com>
2026-04-09 13:58:12 -0700

    update home and practice UI, improve layout and user guidance

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,33 +4,63 @@ import styles from "./Login.module.css";
 import "../scss/styles.scss";
 
 export default function login() {
-    return (
-        <div className="d-flex justify-content-center align-items-center vh-100 bg-secondary">
-            <div className="w-75">
-                <div className="d-flex justify-content-between align-items-center mb-3">
-                    <button className="btn btn-success position-absolute top-0 start-0">
-                        <Link className="text-white" to="/">
-                            ← Back
-                        </Link>
-                    </button>
-                    <span className="position-absolute top-0 start-50 translate-middle-x d-flex align-items-center">
-                        <img src={logo} alt="logo" style={{ width: '128px' }}></img>
-                        <h1 className="d-inline-block ms-3">iagnostic Training Tool</h1>
-                    </span>
-                </div>
-
-                <form action="">
-                    <div className="form-floating mb-3">
-                        <input name="username" type="text" className="form-control" id="floatingUsername" placeholder="Username:" required />
-                        <label htmlFor="floatingUsername">Username:</label>
-                    </div>
-                    <div className="form-floating mb-3">
-                        <input name="password" type="password" className="form-control" id="floatingPassword" placeholder="Password:" required />
-                        <label htmlFor="floatingPassword">Password:</label>
-                    </div>
-                    <button type="submit" className="btn btn-primary w-100">Login</button>
-                </form>
-            </div>
+  return (
+    <div
+      className={`d-flex justify-content-center align-items-center vh-100 ${styles.pageBackground}`}
+    >
+      <div className="w-50">
+        <div className="d-flex justify-content-between align-items-center mb-3">
+          <span className="position-absolute top-0 start-50 translate-middle-x  mt-5 d-flex align-items-center">
+            <img src={logo} alt="logo" style={{ width: "128px" }}></img>
+            <h1 className="d-inline-block ms-3">iagnostic Training Tool</h1>
+          </span>
         </div>
-    )
+
+        <form action="">
+          <div className="form-floating mb-3">
+            <input
+              name="username"
+              type="text"
+              className="form-control"
+              id="floatingUsername"
+              placeholder="Username:"
+              required
+            />
+            <label htmlFor="floatingUsername">Username:</label>
+          </div>
+          <div className="form-floating mb-3">
+            <input
+              name="password"
+              type="password"
+              className="form-control"
+              id="floatingPassword"
+              placeholder="Password:"
+              required
+            />
+            <label htmlFor="floatingPassword">Password:</label>
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary w-50 d-block mx-auto fs-4 fw-bold"
+          >
+            Login
+          </button>
+
+          {/* Temporary buttons */}
+          <div className="d-flex flex-column gap-3 mt-4">
+            <button className="btn btn-primary w-50">
+              <Link className="text-white" to="/menu">
+                Open the Menu
+              </Link>
+            </button>
+            <button className="btn btn-primary w-50">
+              <Link className="text-white" to="/">
+                ← Back to the Main Page
+              </Link>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,28 @@
-import { Link } from 'react-router';
-import styles from '../App.module.css';
+import { Link } from "react-router";
+import styles from "../App.module.css";
+import logo from "../assets/logo.png";
 
 export default function Home() {
-    return (
-        <div className={styles.page}>
-            <h1 className={styles.title}>Main Page</h1>
-            <Link to="/calculator" className={styles.openBtn}>
-                Open Calculator
-            </Link>
-            <Link to="/login" className={styles.openBtn}>
-                Open Login
-            </Link>
-            <Link to="/menu" className={styles.openBtn}>
-                Open Menu
-            </Link>
-        </div>
-    );
+  return (
+    <div className={styles.page}>
+      <img className="w-25" src={logo} alt="logo"></img>
+      <h1 className={styles.title}>Welcome to Diagnostic Training Tool</h1>
+      <h4 className="text-center text-muted mb-4">
+        Practice diagnosing patients in a simulated environment. Login in to
+        bigin your session.
+      </h4>
+      {/* <Link to="/calculator" className={styles.openBtn}>
+          Open Calculator
+        </Link> */}
+      <button className="btn btn-primary w-25 d-block mx-auto fs-4 fw-bold ">
+        <Link to="/login" className="text-white text-decoration-none d-block">
+          Log In
+        </Link>
+      </button>
+
+      {/* <Link to="/menu" className={styles.openBtn}>
+          Open Menu
+        </Link> */}
+    </div>
+  );
 }
