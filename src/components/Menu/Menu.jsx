a7437533f42eb13f088c6aaebad4596b6e0317ee
Eddie Reyes <108855058+eddie-reyes@users.noreply.github.com>
2026-04-24 10:16:39 -0700

    fixed styling for menu page

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -13,46 +13,34 @@ export default function Menu() {
                     style={{ width: '50%' }}
                 >
                     <img className="w-25" src={logo} alt="logo"></img>
-                    <button className="btn btn-lg btn-primary w-25">
-                        <Link to="/practice" className={styles.link}>
-                            Start New Session
-                        </Link>
-                    </button>
-                    <button className="btn btn-lg btn-primary w-25">
-                        <Link to="/sim" className={styles.link}>
-                            Resume Session
-                        </Link>
-                    </button>
-                    <button className="btn btn-lg btn-primary w-25">
-                        <Link to="/history" className={styles.link}>
-                            View History
-                        </Link>
-                    </button>
-                    <button className="btn btn-lg btn-danger w-25">
-                        <Link to="/" className={styles.link}>
-                            Back To Main
-                        </Link>
-                    </button>
+                    
+                    <Link to="/practice" className="btn btn-lg btn-primary w-25">
+                        Start New Session
+                    </Link>
+                    <Link to="/practice" className="btn btn-lg btn-primary w-25">
+                        Resume Session
+                    </Link>
+                    <Link to="/history" className="btn btn-lg btn-primary w-25">
+                        View History
+                    </Link>
+                    <Link to="/" className="btn btn-lg btn-danger w-25">
+                        Back to Main
+                    </Link>
+                    
                 </div>
 
-                <div className=" bg-dark text-white" style={{ width: '50%' }}>
+                <div className=" bg-dark text-white d-flex flex-column justify-content-between align-items-center" style={{ width: '50%' }}>
                     <p className="display-4 m-5">Welcome Back, [firstName] [lastName]</p>
-                    <div
-                        className="d-flex flex-column justify-content-center align-items-center"
-                        style={{ marginTop: '10rem' }}
+                    <img src={pfp} className="rounded-circle" alt="pfp"></img>
+                    <p className="h3 mt-3">Kaiser Permanente, M.D.</p>
+                    <p className="h3">Surgeon</p>
+                    <button
+                        className="btn btn-lg btn-success  w-25 m-5"
                     >
-                        <img src={pfp} className="rounded-circle" alt="pfp"></img>
-                        <p className="h3 mt-3">Kaiser Permanente, M.D.</p>
-                        <p className="h3 mt-3">Surgeon</p>
-                        <button
-                            className="btn btn-lg btn-success  w-25"
-                            style={{ marginTop: '10rem' }}
-                        >
-                            <Link to="/stats" className={styles.link}>
-                                View Statistics
-                            </Link>
-                        </button>
-                    </div>
+                        <Link to="/stats" className={styles.link}>
+                            View Statistics
+                        </Link>
+                    </button>
                 </div>
             </div>
         </>
