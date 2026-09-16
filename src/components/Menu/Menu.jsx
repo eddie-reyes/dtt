b8d5c2c0d6c504eef9ccbf152a46b11d82dd75d1
BraveSword123 <annabelle.gyeniba.moore@gmail.com>
2026-09-15 20:00:51 -0700

    refactor Menu component layout and update button widths for better responsiveness

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -23,19 +23,22 @@ export default function Menu() {
         <>
             <div className="d-flex" style={{ height: '100vh', width: '100vw' }}>
                 <div
-                    className="d-flex flex-row-reverse justify-content-center align-items-end gap-5 p-3 "
-                    style={{ width: '50%' }}
-                >
+                    className="d-flex flex-row-reverse justify-content-center align-items-flex-end gap-5 p-3 "
+                    /* style={{ width: '50%' }} */ 
+                    style={{background: 'var(--seaform-grey)',
+                            /* height: '50px', */ 
+
+                    }}
+                >   
                     {/* <img className="w-25" src={logo} alt="logo"></img> */}
 
                     <Link
                         to="/practice"
-                        className="btn btn-lg  w-25"
+                        className="btn btn-lg  w-50"  
                         style={{
                             background: 'var(--default-btn)',
                             color: 'var(--light-text)',
                             padding: '12px',
-                            borderRadius: '12px',
                             border: '1px solid var(--grey-border)',
                         }}
                     >
@@ -43,7 +46,7 @@ export default function Menu() {
                     </Link>
                     <Link
                         to="/practice"
-                        className="btn btn-lg  w-25"
+                        className="btn btn-lg  w-50"
                         style={{
                             background: 'var(--default-btn)',
                             color: 'var(--light-text)',
@@ -55,7 +58,7 @@ export default function Menu() {
                     </Link>
                     <Link
                         to="/history"
-                        className="btn btn-lg  w-25"
+                        className="btn btn-lg  w-50"
                         style={{
                             background: 'var(--default-btn)',
                             color: 'var(--light-text)',
@@ -66,14 +69,14 @@ export default function Menu() {
                         View History
                     </Link>
                     <Link to="/" 
-                           className="btn btn-lg btn-danger w-25"
+                           className="btn btn-lg btn-danger w-50"
                             style={{
                             background:'var(--special-btn)',
                             color: 'var(--light-text)',
                             border: '1px solid var(--light-border)',
                         }}
                     >
-                        Back to Main
+                        Log Out
                     </Link>
                 </div>
 
